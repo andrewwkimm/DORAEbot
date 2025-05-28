de1c06c2710ca02da2706363499e44299b29c976
andrewwkimm <andrewkimka@gmail.com>
2025-05-28 00:44:29 +0000

    Remove redundant test

--- tests/test_modele.py
+++
@@ -1,8 +0,0 @@
-"""Tests for DORAEbot."""
-
-from DORAEbot import __version__
-
-
-def test_version() -> None:
-    """Tests that version is set to expected value."""
-    assert __version__ == "0.1.0"
